99e1ba0d0f244bc6cb23a7209759f0cd75be9f29
[dyad] <vasfnegocios@gmail.com>
2025-11-05 10:44:06 -0300

    [dyad] Setting default test plate to GZG1097 - wrote 1 file(s) + extra files edited outside of Dyad

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,8 +1,9 @@
 import { fetchAnttByPlate } from '../scraper/antt.js';
 
-const placa = process.argv[2] || 'JAA4B41';
+const placa = process.argv[2] || 'GZG1097';
 
 (async () => {
+  console.log(`Iniciando teste para a placa: ${placa}`);
   const data = await fetchAnttByPlate(placa);
   console.log(JSON.stringify({ placa, ...data }, null, 2));
 })();
